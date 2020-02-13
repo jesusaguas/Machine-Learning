cca568a83c8264e8eb26644f31f0899935665f3a
Jesus Aguas <736935@unizar.es>
2020-02-13 14:51:39 -0800

    Removed  directory

--- Workspace/clasificacionBayesiana.m
+++
@@ -1,9 +0,0 @@
-function yhat = clasificacionBayesiana(modelo, X)
-% Con los modelos entrenados, predice la clase para cada muestra X
-    h=[];
-    for i=1:10
-        h=[h gaussLog(modelo{i}.mu, modelo{i}.Sigma, X)];
-    end
-    [A,yhat] = max(h');
-end
-
